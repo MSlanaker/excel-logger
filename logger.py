174ae5dc71c8bebd1530b8ec6f704c059729b3ce
Matthew Slanaker <matt.slanaker@gmail.com>
2022-02-13 18:06:17 -0500

    updating the logger.py

--- logger.py
+++ logger.py
@@ -1,44 +1,47 @@
+# Importing needed modules ################################################
+
 from openpyxl import Workbook, load_workbook
 from datetime import datetime
 import os
 import logging
 
+# Setting up logging configurations ######################################
 logging.basicConfig(filename="value_log.log", level=logging.INFO,
                     format="%(message)s")
 
+# File input
 file_name = input(
-    "Please enter the name of the file you would like to search:")
+    "Please enter the name of the file you would like to search: ")
 
-wb = load_workbook(file_name)
+# Path for the file and loading up the given excel file
+file = 'C:\\Users\\Owner\\OneDrive\\Desktop\\excel-logger\\Call Spreadsheets\\' + file_name
+wb = load_workbook(file)
 ws = wb.worksheets[0]
+
+# Getting the month and date from the file name ##################################
 
 # Extract the month and year from the file name to compare against the sheet data
 file_name_lst = (os.path.splitext(file_name)[0].split("_"))
-print(file_name_lst)
 
 # Convert back to a string without the ".xlsx" or "_"
 convert_string = " ".join([str(item) for item in file_name_lst])
 
-
 # Isolate the part of the string that has the month and year
 month_year_string = convert_string[23:]
-
-print(month_year_string)
 
 # Conver the month and year to the same format as the date value on the sheet
 formatted_date = datetime.strptime(month_year_string, "%B %Y")
 
-print(formatted_date)
 
+worksheet_rows = list(ws.iter_rows(values_only=True))
 
-worksheet_rows_1st_sheet = list(ws.iter_rows(values_only=True))
 
 # For loop to go through each row and compare the date value to the value pulled from the file name
 # If those values match, then it prints the cell values as a list
-for row in worksheet_rows_1st_sheet:
+for row in worksheet_rows:
     if row[0] == formatted_date:
         row_data = [row for row in row if row != None]
-        print(row_data)
+        print("Finding your information.")
 
 # Take the raw values and convert them into more readable values for the log
 # Convert the date to a readable string and turn the decimals back to percentages
@@ -49,10 +52,8 @@ fcr = row_data[3] * 100
 dsat = row_data[4] * 100
 csat = row_data[5] * 100
 
-print(month_converted, calls_offered, abandon_after_30, fcr, dsat, csat)
-
 # Log the info to the value_log.log file
-logging.info("Date: " + month_converted)
+logging.info("Month: " + month_converted)
 logging.info("Calls Offered: " + str(calls_offered))
 logging.info("Abandon After 30: " + str(abandon_after_30) + "%")
 logging.info("FCR: " + str(fcr) + "%")
