9cd2bf5c7a99a4f29e26049ad6d66c0bf05355d0
Matthew Slanaker <matt.slanaker@gmail.com>
2022-02-12 19:01:59 -0500

    converting date string to an actual date value

--- logger.py
+++ logger.py
@@ -1,24 +1,36 @@
 from openpyxl import Workbook, load_workbook
 from datetime import datetime
+import os
 
 file_name = "expedia_report_monthly_january_2018.xlsx"
 
-file_month_year = file_name[23:35].replace("_", " ")
-
 wb = load_workbook(file_name)
-ws = wb.active
+ws = wb.worksheets[0]
 
-dateTimeObj = ws['A12'].value
+# Extract the month and year from the file name to compare against the sheet data
+file_name_lst = (os.path.splitext(file_name)[0].split("_"))
+print(file_name_lst)
 
-dateStr = dateTimeObj.strftime("%B %Y").lower()
-
-print(dateStr)
-print(file_month_year)
-
-if file_month_year == dateStr:
-    print("Oh my goooodness")
+# Convert back to a string without the ".xlsx" or "_"
+convert_string = " ".join([str(item) for item in file_name_lst])
 
 
-first_row = ws["A1":"F13"].value
+# Isolate the part of the string that has the month and year
+month_year_string = convert_string[23:]
 
-print(first_row)
+print(month_year_string)
+
+# Conver the month and year to the same format as the date value on the sheet
+formatted_date = datetime.strptime(month_year_string, "%B %Y")
+
+print(formatted_date)
+
+
+worksheet_rows_1st_sheet = list(ws.iter_rows(values_only=True))
+
+# For loop to go through each row and compare the date value to the value pulled from the file name
+# If those values match, then it prints the cell values as a list
+for row in worksheet_rows_1st_sheet:
+    if row[0] == formatted_date:
+        row_data = [row for row in row if row != None]
+        print(row_data)
